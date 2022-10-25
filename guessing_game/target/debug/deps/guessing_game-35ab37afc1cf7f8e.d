054adb4ee1d0bbeb07e0726d153f74b8727e049d
韩侠 <shx1432543716@163.com>
2022-10-25 17:45:43 +0800

    测试第一部分代码

---
+++ guessing_game/target/debug/deps/guessing_game-35ab37afc1cf7f8e.d
@@ -0,0 +1,5 @@
+/Applications/XAMPP/xamppfiles/htdocs/Github/Rust/guessing_game/target/debug/deps/guessing_game-35ab37afc1cf7f8e.rmeta: src/main.rs
+
+/Applications/XAMPP/xamppfiles/htdocs/Github/Rust/guessing_game/target/debug/deps/guessing_game-35ab37afc1cf7f8e.d: src/main.rs
+
+src/main.rs:
--- guessing_game/target/debug/deps/guessing_game-b643d824a68552f6.d
+++ guessing_game/target/debug/deps/guessing_game-b643d824a68552f6.d
@@ -1,5 +1,5 @@
-/Applications/XAMPP/xamppfiles/htdocs/practice/RUST/guessing_game/target/debug/deps/guessing_game-b643d824a68552f6: src/main.rs
+/Applications/XAMPP/xamppfiles/htdocs/Github/Rust/guessing_game/target/debug/deps/guessing_game-b643d824a68552f6: src/main.rs
 
-/Applications/XAMPP/xamppfiles/htdocs/practice/RUST/guessing_game/target/debug/deps/guessing_game-b643d824a68552f6.d: src/main.rs
+/Applications/XAMPP/xamppfiles/htdocs/Github/Rust/guessing_game/target/debug/deps/guessing_game-b643d824a68552f6.d: src/main.rs
 
 src/main.rs:
